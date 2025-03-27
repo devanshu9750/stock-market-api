ece3be3024f0ef9b59699397ac6cd7a925eb9663
devanshu-fitterfly <107918529+fitterfly-devanshu@users.noreply.github.com>
2025-03-27 15:25:18 +0530

    add: try catch in redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,10 +4,14 @@ let redisClient = null;
 
 const connectRedis = async () => {
   if (!redisClient) {
-    redisClient = createClient();
-    redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
-    await redisClient.connect();
-    console.log('✅ Redis Connected Successfully');
+    try {
+      redisClient = createClient();
+      redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
+      await redisClient.connect();
+      console.log('✅ Redis Connected Successfully');
+    } catch (err) {
+      console.error('❌ Redis Connection Error:', err);
+    }
   }
 }
 
