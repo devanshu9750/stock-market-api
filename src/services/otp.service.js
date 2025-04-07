953e13f5f2e5db6b53712e8e418336efe0e0b36d
devanshu-fitterfly <107918529+fitterfly-devanshu@users.noreply.github.com>
2025-04-07 15:24:31 +0530

    fix: naming

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -5,16 +5,16 @@ const otpService = {
     generateOTP: (mobileNumber) => {
         try {
             let otp = crypto.randomInt(100000, 1000000).toString();
-            let client = getRedisClient();
-            client.set(`otp:${mobileNumber}`, otp, { EX: 60 });
+            let redisClient = getRedisClient();
+            redisClient.set(`otp:${mobileNumber}`, otp, { EX: 60 });
             return otp;
         } catch (error) { throw new Error(error); }
     },
 
     verifyOTP: async (mobileNumber, otp) => {
         try {
-            let client = getRedisClient();
-            let storedOTP = await client.get(`otp:${mobileNumber}`);
+            let redisClient = getRedisClient();
+            let storedOTP = await redisClient.get(`otp:${mobileNumber}`);
             return storedOTP === otp;
         } catch (error) { throw new Error(error); }
     }
